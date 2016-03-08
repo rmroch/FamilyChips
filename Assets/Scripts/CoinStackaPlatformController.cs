abd483db093fac22edbb343747ee676ec0d33de9
Robert Mroch <Robert.Mroch@Epsilon.com>
2016-03-08 10:50:41 -0600

    remove direct access to transform to fix physics

--- Assets/Scripts/CoinStackaPlatformController.cs
+++ Assets/Scripts/CoinStackaPlatformController.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class CoinStackaPlatformController : MonoBehaviour {
     public float Speed = .05f;
+    private Rigidbody _rigidbody;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    _rigidbody = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if(Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + Speed, -2.53f, 0);
+            _rigidbody.AddForce(new Vector3(20f, 0, 0));
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - Speed, -2.53f, 0);
+            _rigidbody.AddForce(new Vector3(-20f, 0, 0));
         }
-        gameObject.transform.position = new Vector3(gameObject.transform.position.x, -2.53f, 0);
-        gameObject.transform.rotation = Quaternion.identity;
     }
 }
